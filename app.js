8ca917eceb4155bb7cdfc8608418a87da0fb4094
Charlie <chen_610146@hotmail.com>
2019-05-27 20:53:02 +0800

    Finish

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const time = require('./middleware')
+
+// 列出全部 Todo
+app.get('/', time, (req, res) => {
+  res.send('列出全部 Todo')
+})
+
+// 新增一筆 Todo 頁面
+app.get('/new', time, (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+// 顯示一筆 Todo 的詳細內容
+app.get('/:id', time, (req, res) => {
+  res.send('顯示一筆 Todo')
+})
+
+// 新增一筆  Todo
+app.post('/', time, (req, res) => {
+  res.send('新增一筆  Todo')
+})
+
+app.delete('/:id/delete', time, (req, res) => {
+  res.send('刪除 Todo')
+})
+
+
+app.listen(port, () => {
+  console.log('Express running')
+})
